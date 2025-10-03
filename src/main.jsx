5c473917dd51449302fefd0a051e1dd61895dd7c
Avelino <2321267@ub.edu.ph>
2025-10-03 17:35:09 +0800

    Second Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,16 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import '/MyStyle.scss';
 
-function MissedGoal() {
-  return <h1>MISSED!</h1>;
-}
-
-function MadeGoal() {
-  return <h1>Goal!</h1>;
-}
-
-function Goal(props) {
-  const isGoal = props.isGoal;
-  if (isGoal) {
-    return <MadeGoal/>;
-  }
-  return <MissedGoal/>;
+function MyHeader() {
+  return (
+    <div>
+    <h1>My Header 1</h1>
+    <h2>My Header 2</h2>
+    <h3>My Header 3</h3>
+    </div>
+  );
 }
 
 createRoot(document.getElementById('root')).render(
-  <Goal isGoal={false} />
+  <MyHeader />
 );
